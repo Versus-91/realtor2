b1203f5ec562b38a219d97e97fd956a4c7d35e43
ghader <gharibyan.ghader@gmail.com>
2020-12-01 18:44:15 +0330

    new bottom nav

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,20 +1,16 @@
-import 'package:boilerplate/models/Nvigation/bottom_nav.dart';
+import 'package:boilerplate/data/sharedpref/constants/preferences.dart';
+import 'package:boilerplate/routes.dart';
+import 'package:boilerplate/stores/language/language_store.dart';
 import 'package:boilerplate/stores/post/post_store.dart';
-import 'package:boilerplate/stores/user/user_store.dart';
-import 'package:boilerplate/ui/home/tabs/user_screen.dart';
-import 'package:boilerplate/ui/home/tabs/search_tab_screen.dart';
-import 'package:boilerplate/ui/post/createPost.dart';
-import 'package:boilerplate/ui/post/post.dart';
-import 'package:boilerplate/ui/profile/favorites_screen.dart';
-import 'package:boilerplate/ui/profile/pages/info.dart';
+import 'package:boilerplate/stores/theme/theme_store.dart';
 import 'package:boilerplate/utils/locale/app_localization.dart';
-import 'package:boilerplate/widgets/empty_app_bar_widget.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:boilerplate/widgets/progress_indicator_widget.dart';
+import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:material_dialog/material_dialog.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../../data/sharedpref/constants/preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -22,79 +18,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  PersistentTabController _controller;
-  bool _hideNavBar;
-
-  GlobalKey globalKey = new GlobalKey(debugLabel: 'btm_nav_bar');
   //stores:---------------------------------------------------------------------
-  UserStore _userStore;
   PostStore _postStore;
-
-  int _screenNumber = 0;
-  List<BottomNav> navItems;
-
-  bool loggedIn = false;
-  Future<Null> getSharedPrefs() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      loggedIn = prefs.getBool(Preferences.is_logged_in) ?? false;
-    });
-  }
-
-  List<PersistentBottomNavBarItem> _navBarsItems() {
-    return [
-      PersistentBottomNavBarItem(
-        icon: Icon(Icons.home),
-        title: "Home",
-        activeColor: Colors.blue,
-        inactiveColor: Colors.grey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(Icons.search),
-        title: ("Search"),
-        activeColor: Colors.teal,
-        inactiveColor: Colors.grey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(Icons.add),
-        title: ("Add"),
-        activeColor: Colors.deepOrange,
-        inactiveColor: Colors.grey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(Icons.favorite),
-        title: ("Likes"),
-        activeColor: Colors.deepOrange,
-        inactiveColor: Colors.grey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(Icons.settings),
-        title: ("Settings"),
-        activeColor: Colors.indigo,
-        inactiveColor: Colors.grey,
-      ),
-    ];
-  }
-
-  List<Widget> _buildScreens() {
-    return [
-      UserScreen(
-        userStore: _userStore,
-        postStore: _postStore,
-      ),
-      SearchTabScreen(),
-      CreatePostScreen(),
-      FavoritesScreen(),
-      SettingsScreen(),
-    ];
-  }
+  ThemeStore _themeStore;
+  LanguageStore _languageStore;
 
   @override
   void initState() {
     super.initState();
-    _controller = PersistentTabController(initialIndex: 0);
-    _hideNavBar = false;
-    getSharedPrefs();
   }
 
   @override
@@ -102,88 +33,219 @@ class _HomeScreenState extends State<HomeScreen> {
     super.didChangeDependencies();
 
     // initializing stores
-
-    _userStore = Provider.of<UserStore>(context);
+    _languageStore = Provider.of<LanguageStore>(context);
+    _themeStore = Provider.of<ThemeStore>(context);
     _postStore = Provider.of<PostStore>(context);
+
+    // check to see if already called api
+    if (!_postStore.loading) {
+      _postStore.getPosts();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PersistentTabView(
-        controller: _controller,
-        screens: _buildScreens(),
-        confineInSafeArea: true,
-        itemCount: 5,
-        backgroundColor: Colors.white,
-        handleAndroidBackButtonPress: true,
-        resizeToAvoidBottomInset: true,
-        stateManagement: true,
-        hideNavigationBarWhenKeyboardShows: true,
-        hideNavigationBar: _hideNavBar,
-        decoration: NavBarDecoration(
-          colorBehindNavBar: Colors.indigo,
-        ),
-        popAllScreensOnTapOfSelectedTab: true,
-        itemAnimationProperties: ItemAnimationProperties(
-          duration: Duration(milliseconds: 400),
-          curve: Curves.ease,
-        ),
-        screenTransitionAnimation: ScreenTransitionAnimation(
-          animateTabTransition: true,
-          curve: Curves.ease,
-          duration: Duration(milliseconds: 200),
-        ),
-        // customWidget: BottomNavStyle1(
-        //   items: _navBarsItems(),
-        //   onItemSelected: (index) {
-        //     setState(() {
-        //       _controller.index = index; // THIS IS CRITICAL!! Don't miss it!
-        //     });
-        //   },
-        //   selectedIndex: _controller.index,
-        // ),
-        items: _navBarsItems(),
-        onItemSelected: (index) {
-          setState(() {
-            _controller.index = index; // THIS IS CRITICAL!! Don't miss it!
-          });
-        },
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
 
-        navBarStyle:
-            NavBarStyle.style1, // Choose the nav bar style with this property
+  // app bar methods:-----------------------------------------------------------
+  Widget _buildAppBar() {
+    return AppBar(
+      title: Text(AppLocalizations.of(context).translate('home_tv_posts')),
+      actions: _buildActions(context),
+    );
+  }
+
+  List<Widget> _buildActions(BuildContext context) {
+    return <Widget>[
+      _buildLanguageButton(),
+      _buildThemeButton(),
+      _buildLogoutButton(),
+    ];
+  }
+
+  Widget _buildThemeButton() {
+    return Observer(
+      builder: (context) {
+        return IconButton(
+          onPressed: () {
+            _themeStore.changeBrightnessToDark(!_themeStore.darkMode);
+          },
+          icon: Icon(
+            _themeStore.darkMode ? Icons.brightness_5 : Icons.brightness_3,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildLogoutButton() {
+    return IconButton(
+      onPressed: () {
+        SharedPreferences.getInstance().then((preference) {
+          preference.setBool(Preferences.is_logged_in, false);
+          Navigator.of(context).pushReplacementNamed(Routes.login);
+        });
+      },
+      icon: Icon(
+        Icons.power_settings_new,
       ),
     );
   }
 
+  Widget _buildLanguageButton() {
+    return IconButton(
+      onPressed: () {
+        _buildLanguageDialog();
+      },
+      icon: Icon(
+        Icons.language,
+      ),
+    );
+  }
+
+  // body methods:--------------------------------------------------------------
+  Widget _buildBody() {
+    return Stack(
+      children: <Widget>[
+        _handleErrorMessage(),
+        _buildMainContent(),
+      ],
+    );
+  }
+
+  Widget _buildMainContent() {
+    return Observer(
+      builder: (context) {
+        return _postStore.loading
+            ? CustomProgressIndicatorWidget()
+            : Material(child: _buildListView());
+      },
+    );
+  }
+
+  Widget _buildListView() {
+    return _postStore.postList != null
+        ? ListView.separated(
+            itemCount: _postStore.postList.posts.length,
+            separatorBuilder: (context, position) {
+              return Divider();
+            },
+            itemBuilder: (context, position) {
+              return _buildListItem(position);
+            },
+          )
+        : Center(
+            child: Text(
+              AppLocalizations.of(context).translate('home_tv_no_post_found'),
+            ),
+          );
+  }
+
+  Widget _buildListItem(int position) {
+    return ListTile(
+      dense: true,
+      leading: Icon(Icons.cloud_circle),
+      title: Text(
+        '${_postStore.postList.posts[position].title}',
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        softWrap: false,
+        style: Theme.of(context).textTheme.title,
+      ),
+      subtitle: Text(
+        '${_postStore.postList.posts[position].description}',
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        softWrap: false,
+      ),
+    );
+  }
+
+  Widget _handleErrorMessage() {
+    return Observer(
+      builder: (context) {
+        if (_postStore.errorStore.errorMessage.isNotEmpty) {
+          return _showErrorMessage(_postStore.errorStore.errorMessage);
+        }
+
+        return SizedBox.shrink();
+      },
+    );
+  }
+
   // General Methods:-----------------------------------------------------------
+  _showErrorMessage(String message) {
+    Future.delayed(Duration(milliseconds: 0), () {
+      if (message != null && message.isNotEmpty) {
+        FlushbarHelper.createError(
+          message: message,
+          title: AppLocalizations.of(context).translate('home_tv_error'),
+          duration: Duration(seconds: 3),
+        )..show(context);
+      }
+    });
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
-}
-
-class ContestTabHeader extends SliverPersistentHeaderDelegate {
-  ContestTabHeader(
-    this.searchUI,
-  );
-  final Widget searchUI;
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return searchUI;
+    return SizedBox.shrink();
   }
 
-  @override
-  double get maxExtent => 52.0;
+  _buildLanguageDialog() {
+    _showDialog<String>(
+      context: context,
+      child: MaterialDialog(
+        borderRadius: 5.0,
+        enableFullWidth: true,
+        title: Text(
+          AppLocalizations.of(context).translate('home_tv_choose_language'),
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ),
+        headerColor: Theme.of(context).primaryColor,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        closeButtonColor: Colors.white,
+        enableCloseButton: true,
+        enableBackButton: false,
+        onCloseButtonClicked: () {
+          Navigator.of(context).pop();
+        },
+        children: _languageStore.supportedLanguages
+            .map(
+              (object) => ListTile(
+                dense: true,
+                contentPadding: EdgeInsets.all(0.0),
+                title: Text(
+                  object.language,
+                  style: TextStyle(
+                    color: _languageStore.locale == object.locale
+                        ? Theme.of(context).primaryColor
+                        : _themeStore.darkMode
+                            ? Colors.white
+                            : Colors.black,
+                  ),
+                ),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  // change user language based on selected locale
+                  _languageStore.changeLanguage(object.locale);
+                },
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
 
-  @override
-  double get minExtent => 52.0;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
+  _showDialog<T>({BuildContext context, Widget child}) {
+    showDialog<T>(
+      context: context,
+      builder: (BuildContext context) => child,
+    ).then<void>((T value) {
+      // The value passed to Navigator.pop() or null.
+    });
   }
 }
