cb4efb1aca71adbfecad9c69423780bf8061895f
ghader <gharibyan.ghader@gmail.com>
2020-12-01 16:19:58 +0330

    new bottom navigation

--- lib/ui/profile/pages/info.dart
+++ lib/ui/profile/pages/info.dart
@@ -87,11 +87,6 @@ class _SettingsScreenState extends State<SettingsScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          AppLocalizations.of(context).translate('settings'),
-        ),
-      ),
       body: SettingsList(
         backgroundColor: Colors.white,
         sections: [
@@ -173,7 +168,9 @@ class _SettingsScreenState extends State<SettingsScreen>
                 enabled: true,
                 leading: Icon(Icons.notifications_active),
                 switchValue: true,
-                onToggle: (value) {},
+                onToggle: (value) {
+                  print('object');
+                },
               ),
             ],
           ),
